e76417ac9ac582e385ecd7263d7aacb603082de4
TheChosenOne2105 <simon.flaisch@student.reutlingen-university.de>
2022-05-25 14:49:00 +0200

    Erster Entwurf eines Serversockets mit möglicher Kommunikation

---
+++ src/Clienthandler.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+
+public class Clienthandler extends Thread{
+    final Socket client;
+    public Clienthandler(Socket clientsocket){
+        this.client = clientsocket;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            clienthandler(client);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void clienthandler(Socket clientsocket) throws IOException {
+        InputStream inputStream = clientsocket.getInputStream();
+        OutputStream outputStream = clientsocket.getOutputStream();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        outputStream.write("Connected to Server!\n\r".getBytes());
+        String Userinput;
+        while ((Userinput = bufferedReader.readLine()) != null){
+            String msg =Userinput ;
+            String output = "You Typed: " + msg + "\n\r";
+            outputStream.write(output.getBytes());
+            if(msg.equalsIgnoreCase("quit")){
+                break;
+            }
+
+        }
+        outputStream.write("\n\rYou disconnected from the Server!".getBytes());
+        clientsocket.close();
+
+
+    }
+}
