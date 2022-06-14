a63502f0459e7f2b23c1bcc5622bc69fd85bd4f8
TheChosenOne2105 <simon.flaisch@student.reutlingen-university.de>
2022-06-14 12:02:10 +0200

    Geschafft!

--- src/Client.java
+++ src/Client.java
@@ -1,39 +1,61 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 //Basierend auf dem Code von diesem Tutorial: https://www.youtube.com/watch?v=gLfuZrrfKes
 public class Client {
+    private JButton Send;
+    private JTextArea textArea2;
+    private JTextField typeMessage;
+    private JPanel Panel;
+
+    static ImageIcon image= new ImageIcon("src/default/SpeedchatIcon.jpeg");
+    static JFrame frame= new JFrame();
     private Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
+
 
     public Client(Socket socket){
         try {
             this.socket = socket;
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.bufferedWriter= new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            textArea2.setEditable(false);
+            Send.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (e.getSource() == Send) {
+                        textArea2.append(typeMessage.getText());
+                        String messageToSend = typeMessage.getText();
+                        try {
+                            textArea2.append("\n");
+                            bufferedWriter.write(messageToSend);
+                            bufferedWriter.newLine();
+                            bufferedWriter.flush();
+                            typeMessage.setText("");
+                        } catch (IOException ex) {
+                            closeEverything(socket, bufferedReader, bufferedWriter);
+                        }
+                    }
+
+
+                }
+            });
         } catch (IOException e) {
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
-
-
-    public void sendMessage(Scanner scanner) {
-        try {
-
-            while (socket.isConnected()) {
-                String messageToSend = scanner.nextLine();
-                bufferedWriter.write(messageToSend);
-                bufferedWriter.newLine();
-                bufferedWriter.flush();
-            }
-            closeEverything(socket, bufferedReader, bufferedWriter);
-        } catch (IOException e) {
-
-            closeEverything(socket, bufferedReader, bufferedWriter);
-        }
+    public void frame() {
+        frame.setSize(500, 500);
+        frame.setContentPane(Panel);
+        frame.setIconImage(image.getImage());
+        frame.setVisible(true);
+        frame.setTitle("SpeedChat");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 
@@ -41,20 +63,14 @@ public class Client {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                String msgFromGroupChat;
+
 
                 while (socket.isConnected()) {
                     try {
-
-
-                        msgFromGroupChat = bufferedReader.readLine();
-                        if(msgFromGroupChat == null){
-                            closeEverything(socket,bufferedReader, bufferedWriter);
+                        textArea2.append(bufferedReader.readLine());
+                        textArea2.append("\n");
                         }
-                        else{
-                            System.out.println(msgFromGroupChat);
-                        }
-                    } catch (IOException e) {
+                    catch (IOException e) {
 
                         closeEverything(socket, bufferedReader, bufferedWriter);
                     }
@@ -85,12 +101,10 @@ public class Client {
     // Run the program.
     public static void main(String[] args) {
         try {
-            Scanner sc = new Scanner(System.in);
 
             Client client = new Client(new Socket("localhost", 1833));
-
+            client.frame();
             client.listenForMessage();
-            client.sendMessage(sc);
         } catch (UnknownHostException e) {
             System.out.println("Server is unreachable");
         } catch (IOException e) {
